9d94fb9df134e770f99f703f13503e06fbf1b695
Liviu Damaschin <liviu.damaschin@gmail.com>
2017-12-09 22:51:06 +0200

    added BackOffice project

---
+++ MusicIdentificationSystem/MusicIdentificationSystem.Backoffice/Helpers/CommonHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace MusicIdentificationSystem.Backoffice.Helpers
+{
+    public class CommonHelper
+    {
+        public static bool IsValidEmail(string email)
+        {
+            if (String.IsNullOrEmpty(email))
+                return false;
+
+            email = email.Trim();
+            var result = Regex.IsMatch(email, "^(?:[\\w\\!\\#\\$\\%\\&\\'\\*\\+\\-\\/\\=\\?\\^\\`\\{\\|\\}\\~]+\\.)*[\\w\\!\\#\\$\\%\\&\\'\\*\\+\\-\\/\\=\\?\\^\\`\\{\\|\\}\\~]+@(?:(?:(?:[a-zA-Z0-9](?:[a-zA-Z0-9\\-](?!\\.)){0,61}[a-zA-Z0-9]?\\.)+[a-zA-Z0-9](?:[a-zA-Z0-9\\-](?!$)){0,61}[a-zA-Z0-9]?)|(?:\\[(?:(?:[01]?\\d{1,2}|2[0-4]\\d|25[0-5])\\.){3}(?:[01]?\\d{1,2}|2[0-4]\\d|25[0-5])\\]))$", RegexOptions.IgnoreCase);
+            return result;
+        }
+    }
+}
---
+++ MusicIdentificationSystem/MusicIdentificationSystem.Backoffice/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MusicIdentificationSystem.Backoffice.Startup))]
+namespace MusicIdentificationSystem.Backoffice
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
